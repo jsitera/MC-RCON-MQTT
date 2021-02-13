98ee2fd944cb28440a5b3ce3b87884c5152d82b6
root <root@localhost>
2021-02-13 17:05:57 +0100

    docker push only as a comment

--- build-docker.sh
+++ build-docker.sh
@@ -2,5 +2,5 @@ docker build -t sitera/mc-rcon-mqtt:latest .
 
 
 # to upload the image
-docker login --username=sitera --email=email.here
-docker push sitera/mc-rcon-mqtt:latest
+#docker login --username=sitera --email=email.here
+#docker push sitera/mc-rcon-mqtt:latest
